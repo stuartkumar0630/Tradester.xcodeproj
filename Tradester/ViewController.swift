f10034f138fc613c414bd28abdb11bbf099df65d
Stuart Kumar <stuartkumar@stuarts-macbook-pro.local>
2015-12-04 00:47:10 +0000

    Initial Commit

---
+++ Tradester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tradester
+//
+//  Created by Stuart Kumar on 04/12/2015.
+//  Copyright © 2015 Stuart Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
